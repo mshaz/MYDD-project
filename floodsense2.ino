e7d5d93997024d87095a0424470eed533f2a979e
mshaz <azizahsham@yahoo.com>
2016-09-23 13:40:37 +0800

    Add files via upload

---
+++ floodsense2.ino
@@ -0,0 +1,89 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin A3, prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A3, and the outside pins to ~3V and ground.
+  
+  Hardware Required:
+  * MSP-EXP430G2 LaunchPad
+  * 10-kilohm Potentiometer
+  * hook-up wire
+
+  This example code is in the public domain.
+*/
+
+#include <aJSON.h>
+#include "SPI.h"
+#include "WiFi.h"
+
+#include "M2XStreamClient.h"
+
+char ssid[] = "Mydd2016"; //  your network SSID (name)
+char pass[] = "mydd@2016"; // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+char deviceId[] = "ac4da2ac4d9f44c7a81fd944b4484e2c"; // Feed you want to post to
+char m2xKey[] = "f9fa17908e8d09fa121044d07c076e6d"; // Your M2X access key
+char streamName[] = "waterlevel"; // Stream you want to post to
+
+WiFiClient client;
+M2XStreamClient m2xClient(&client, m2xKey);
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600); // msp430g2231 must use 4800
+  
+  // attempt to connect to Wifi network:
+    Serial.print("Attempting to connect to Network named: ");
+    // print the network name (SSID);
+    Serial.println(ssid); 
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    WiFi.begin(ssid, pass);
+    while ( WiFi.status() != WL_CONNECTED) {
+      // print dots while we wait to connect
+      Serial.print(".");
+      delay(300);
+    }
+  
+    Serial.println("\nYou're connected to the network");
+    Serial.println("Waiting for an ip address");
+  
+    while (WiFi.localIP() == INADDR_NONE) {
+      // print dots while we wait for an ip addresss
+      Serial.print(".");
+      delay(300);
+    }
+
+    Serial.println("\nIP Address obtained");
+  
+    // you're connected now, so print out the status  
+    printWifiStatus();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin A3:
+  int sensorValue = analogRead(A3);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  
+    // Send to M2X
+    int response = m2xClient.updateStreamValue(deviceId, streamName, sensorValue);
+    Serial.print("M2X client response code: ");
+    Serial.println(response);
+    
+  delay(1); // delay in between reads for stability
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
